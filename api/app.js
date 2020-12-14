44d3087bde36aca60c47146c4e8bcb66c942786d
Barbara Peric <b.peric10@gmail.com>
2020-12-14 11:30:34 +0100

    WIP: multer setup

--- api/app.js
+++ api/app.js
@@ -7,18 +7,14 @@ const logger = require('morgan');
 const cors = require("cors");
 const formData = require("express-form-data")
 const cloudinary = require('cloudinary')
+const multer = require('multer')
 
 const app = express();
 
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.API_KEY,
-  api_secret: process.env.API_SECRET
-})
-
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 app.use(formData.parse());
 app.use(cors());
 app.use(logger('dev'));
@@ -26,12 +22,55 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('images',express.static(path.join(__dirname, 'images')))
 
-app.post('/image-upload', (req, res) => {
-  const path = Object.values(Object.values(req.files)[0])[0].path
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET
+})
 
-  cloudinary.uploader.upload(path)
-    .then(image => res.json([image]))
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+      cb(null, 'images');
+  },
+  filename: (req, file, cb) => {
+      console.log(file);
+      cb(null, Date.now() + path.extname(file.originalname));
+  }
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype == 'image/jpeg' || file.mimetype == 'image/png') {
+      cb(null, true);
+  } else {
+      cb(null, false);
+  }
+}
+
+const upload = multer({ storage: storage, fileFilter: fileFilter });
+
+app.post('/image-upload', upload.single('image'), (req, res, next) => {
+  try {
+    return res.status(201).json({
+        message: 'File uploaded successfully'
+    });
+  } catch (error) {
+      console.error(error);
+  }
+
+  // cloudinary.uploader.upload(data.image)
+  //   .then(res => {
+  //     res.status(200).send({ 
+  //       message: "successful upload",
+  //       res
+  //     }).catch(err => {
+  //       res.status(500).send({
+  //         message: "Error", 
+  //         err
+  //       })
+  //     })
+  //   })
 
 })
 // catch 404 and forward to error handler

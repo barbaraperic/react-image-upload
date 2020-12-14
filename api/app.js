87c494b12666cdb0354dc74c50b0359d865a20c0
Barbara Peric <b.peric10@gmail.com>
2020-12-14 09:48:56 +0100

    WIP: upload button and funcionality

--- api/app.js
+++ api/app.js
@@ -1,9 +1,11 @@
 var createError = require('http-errors');
+require('dotenv').config()
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require("cors");
+
 
 var testAPI = require('./routes/testAPI');
 
